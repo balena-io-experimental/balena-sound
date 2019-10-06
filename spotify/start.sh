688286d68afe4fcecfc1b2247268592b818652b6
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-10-06 13:45:48 +0200

    Update start.sh

--- spotify/start.sh
+++ spotify/start.sh
@@ -4,4 +4,4 @@ if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
   BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
 fi
 
-./usr/bin/librespot "$BLUETOOTH_DEVICE_NAME"
+./usr/bin/librespot  --name "$BLUETOOTH_DEVICE_NAME" $BACKEND_ARGS --bitrate ${BITRATE} $CACHE_ARGS $VOLUME_ARGS
