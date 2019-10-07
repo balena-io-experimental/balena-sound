6c33bb15277ae1f38ec6d63652fcf6bc1908ed13
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-10-07 16:45:44 +0200

    Delete start.sh

--- airplay/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
-  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
-fi
-
-shairport-sync -a "$BLUETOOTH_DEVICE_NAME"
