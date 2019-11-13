0d7cf1fc871fe8daeb36a2eb1348d23023f2403f
AlexProgrammerDE <abc123-50@gmx.de>
2019-11-13 19:17:24 +0100

    Make raspotify start more clear, add default output volume and add cache
    Change-type: minor

--- spotify/start.sh
+++ spotify/start.sh
@@ -4,4 +4,8 @@ if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
   BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
 fi
 
-./usr/bin/librespot  --name "$BLUETOOTH_DEVICE_NAME" $BACKEND_ARGS --bitrate ${BITRATE} $CACHE_ARGS $VOLUME_ARGS
+# Set the system volume here
+SYSTEM_OUTPUT_VOLUME="${SYSTEM_OUTPUT_VOLUME:-100}"
+
+# Start raspotify
+./usr/bin/librespot  --name "$BLUETOOTH_DEVICE_NAME" --backend alsa --bitrate 320 --cache /var/cache/raspotify --enable-volume-normalisation --linear-volume --initial-volume=$SYSTEM_OUTPUT_VOLUME
