a15dfdab9bc262b4cc45ae4332ccc1158312030b
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-12-16 17:46:39 +0100

    Add code for gpio control with udev and some other things
    Connects-to: #22
    Change-type: minor

--- airplay/start.sh
+++ airplay/start.sh
@@ -4,4 +4,4 @@ if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
   BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
 fi
 
-exec shairport-sync -a "$BLUETOOTH_DEVICE_NAME"
+exec shairport-sync -a "$BLUETOOTH_DEVICE_NAME" | printf "Device is discoverable as \"%s\"\n" "$BLUETOOTH_DEVICE_NAME"
