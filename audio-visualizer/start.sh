0fd35d5e9a22af0e50ee0224ceae5c4d97ca33e1
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-12-19 14:49:46 +0100

    Add glava to start.sh

--- audio-visualizer/start.sh
+++ audio-visualizer/start.sh
@@ -6,10 +6,4 @@ export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo "Starting X in 2 seconds"
 sleep 2
-startx 
-
-while :
-do
-	echo "startx failed, so we will just wait here while you debug!"
-	sleep 30
-done
+startx glava
