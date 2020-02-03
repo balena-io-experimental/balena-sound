c95f21263d0d149d496669c575b73b5e32d9da55
Chris Crocker-White <chriscw@balena.io>
2020-02-03 23:00:12 +0000

    Tweak service names
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- airplay/start.sh
+++ airplay/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
-  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound airplay %s" $(hostname | cut -c -4))
+  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound Airplay %s" $(hostname | cut -c -4))
 fi
 
 exec shairport-sync -a "$BLUETOOTH_DEVICE_NAME" | printf "Device is discoverable as \"%s\"\n" "$BLUETOOTH_DEVICE_NAME"
--- bluetooth-audio/start.sh
+++ bluetooth-audio/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$BLUETOOTH_DEVICE_NAME" ]]; then
-  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound bluetooth %s" $(hostname | cut -c -4))
+  BLUETOOTH_DEVICE_NAME=$(printf "balenaSound %s" $(hostname | cut -c -4))
 fi
 
 # Set the system volume here
