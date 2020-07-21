b9356e7af85e4ee1c7ecbdf97eaa60da9b122123
Tomás Migone <tomas@balena.io>
2020-07-21 14:47:30 -0300

    Fix rpi1/rpi2 bug with multiroom server
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- upnp/start.sh
+++ upnp/start.sh
@@ -15,7 +15,7 @@ if [[ -z "$DEVICE_NAME" ]]; then
 fi
 
 # If multi room is disabled remove audio redirect to fifo pipe
-# Also remove if device is from Pi 1 family, since snapcast server is disabled by default
+# Also remove if device is from Pi 1/2 family, since snapcast server is disabled by default
 if [[ -n $DISABLE_MULTI_ROOM ]] || [[ $BALENA_DEVICE_TYPE == "raspberry-pi" || $BALENA_DEVICE_TYPE == "raspberry-pi2" ]]; then
   rm /root/.asoundrc
 fi
