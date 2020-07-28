56a6df1e6e624784c480a82dbac501972071d2e1
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-07-28 09:17:00 +0200

    Get spotify working
    Still no audio outout. Not sure if its librespot or audio primitive

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
- 
-#Check for incompatible multi room and client-only setting
-if [[ -n $DISABLE_MULTI_ROOM ]] && [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
-  echo “DISABLE_MULTI_ROOM and CLIENT_ONLY_MULTI_ROOM cannot be set simultaneously. Ignoring client-only mode.”
-fi
- 
+
 #Exit service if client-only mode is enabled 
-if [[ -z $DISABLE_MULTI_ROOM ]] && [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
+if [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
   exit 0
 fi
 
@@ -16,24 +11,16 @@ if [[ -z "$DEVICE_NAME" ]]; then
 fi
 
 # Set the system volume here
-SYSTEM_OUTPUT_VOLUME="${SYSTEM_OUTPUT_VOLUME:-75}"
+SYSTEM_OUTPUT_VOLUME="${SYSTEM_OUTPUT_VOLUME:-100}"
 
 # Set the Spotify username and password
-if [[ ! -z "$SPOTIFY_LOGIN" ]] && [[ ! -z "$SPOTIFY_PASSWORD" ]]; then
-  SPOTIFY_CREDENTIALS="--username \"$SPOTIFY_LOGIN\" --password \"$SPOTIFY_PASSWORD\""
+if [[ ! -z "$SOUND_SPOTIFY_LOGIN" ]] && [[ ! -z "$SOUND_SPOTIFY_PASSWORD" ]]; then
+  SOUND_SPOTIFY_CREDENTIALS="--username \"$SPOTIFY_LOGIN\" --password \"$SPOTIFY_PASSWORD\""
   printf "%s\n" "Using Spotify login."
 fi
 
-# Use pipe backend if multi room is enabled
-# Don't pipe for Pi 1/2 family devices since snapcast-server is disabled by default
-if [[ -z $DISABLE_MULTI_ROOM ]] && ! [[ $BALENA_DEVICE_TYPE == "raspberry-pi" || $BALENA_DEVICE_TYPE == "raspberry-pi2" ]]; then
-  SPOTIFY_BACKEND="--backend pipe"
-  SPOTIFY_DEVICE="--device /var/cache/snapcast/snapfifo"
-fi
+# Start pulseaudio for librespot to use
+pulseaudio --start --system=true
 
-# Start raspotify
-if [[ $DISABLE_VOLUME_NORMALISATION == "1" ]]; then
-  exec /usr/bin/librespot --name "$DEVICE_NAME" $SPOTIFY_BACKEND --bitrate 320 --cache /var/cache/raspotify --linear-volume --initial-volume=$SYSTEM_OUTPUT_VOLUME $SPOTIFY_CREDENTIALS $SPOTIFY_DEVICE
-else
-  exec /usr/bin/librespot --name "$DEVICE_NAME" $SPOTIFY_BACKEND --bitrate 320 --cache /var/cache/raspotify --enable-volume-normalisation --linear-volume --initial-volume=$SYSTEM_OUTPUT_VOLUME $SPOTIFY_CREDENTIALS $SPOTIFY_DEVICE
-fi
+# Start librespot
+exec /usr/src/librespot/target/release/librespot --name "$DEVICE_NAME" --bitrate 320 --cache /var/cache/raspotify --enable-volume-normalisation --volume-ctrl linear --initial-volume=$SYSTEM_OUTPUT_VOLUME $SOUND_SPOTIFY_CREDENTIALS --backend pulseaudio
