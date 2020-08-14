442292b9df9b2ab9f5f6fbea16e3169d32cbc676
Tomás Migone <tomas@balena.io>
2020-08-14 16:33:07 -0300

    airplay: add support
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -10,11 +10,7 @@ if [[ -z "$SOUND_DEVICE_NAME" ]]; then
   SOUND_DEVICE_NAME=$(printf "balenaSound Airplay %s" $(hostname | cut -c -4))
 fi
 
-# Use pipe output if multi room is enabled
-# Don't pipe for Pi 1/2 family devices since snapcast-server is disabled by default
-if [[ -z $DISABLE_MULTI_ROOM ]] && ! [[ $BALENA_DEVICE_TYPE == "raspberry-pi" || $BALENA_DEVICE_TYPE == "raspberry-pi2" ]]; then
-  SHAIRPORT_BACKEND="-o pipe -- /var/cache/snapcast/snapfifo"
-fi
-
 # Start AirPlay
-exec shairport-sync -a "$SOUND_DEVICE_NAME" $SHAIRPORT_BACKEND | printf "Device is discoverable as \"%s\"\n" "$SOUND_DEVICE_NAME"
+exec shairport-sync \
+  --name "$SOUND_DEVICE_NAME" \
+  --output pa
--- plugins/upnp/start.sh
+++ plugins/upnp/start.sh
@@ -5,6 +5,7 @@ if [[ -n "$SOUND_DISABLE_UPNP" ]]; then
   exit 0
 fi
 
+# Set the device broadcast name for UPnP
 if [[ -z "$SOUND_DEVICE_NAME" ]]; then
   SOUND_DEVICE_NAME=$(printf "balenaSound UPnP %s" $(hostname | cut -c -4))
 fi
