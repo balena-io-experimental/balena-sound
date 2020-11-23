d3312c2f97a93e4a94c73ab1986965f7f7f8930c
Tomás Migone <tomas@balena.io>
2020-11-23 21:11:07 +0000

    multiroom: update snapcast to v0.22 and various improvements
    - Update snapcast to v0.22.0-r0
    - Replace FIFO stream source with ALSA which significantly reduces CPU and I/O usage (fixes #294)
    - Add SOUND_INPUT_LATENCY and SOUND_OUTPUT_LATENCY env vars (also helps with #294)
    - Reduce multiroom-server image with multi stage builds for a slimmer image
    - Reduce verbose level for both multiroom services
    
    Connects-to: #294
    Change-type: minor
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- core/multiroom/client/start.sh
+++ core/multiroom/client/start.sh
@@ -21,10 +21,7 @@ echo "Target snapcast server: $SNAPSERVER"
 
 # Start snapclient
 if [[ "$MODE" == "MULTI_ROOM" || "$MODE" == "MULTI_ROOM_CLIENT" ]]; then
-  # Start snapclient and filter out those pesky chunk logs
-  # grep filter can be removed when we get snapcast v0.20
-  # see: https://github.com/badaix/snapcast/issues/559#issuecomment-615874719
-  /usr/bin/snapclient --host $SNAPSERVER $LATENCY | grep -v "\[Info\] (Stream) Chunk"
+  /usr/bin/snapclient --host $SNAPSERVER $LATENCY --logfilter *:notice
 else
   echo "Multi-room client disabled. Exiting..."
   exit 0
