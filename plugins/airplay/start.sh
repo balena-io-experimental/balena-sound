e4e5f8c0a1ec82ccaa2ac4e4fada889c96c50416
Tomás Migone <tomas@balena.io>
2021-11-24 11:04:01 -0300

    airplay: fix connection bug with airplay
    Closes: #463, #501, #497, #379, #489, #483
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -17,7 +17,7 @@ echo "Device name: $SOUND_DEVICE_NAME"
 # See: https://github.com/mikebrady/shairport-sync/issues/1054
 # Remove when above issue is addressed
 SOUND_SUPERVISOR_PORT=${SOUND_SUPERVISOR_PORT:-80}
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):$SOUND_SUPERVISOR_PORT"
+SOUND_SUPERVISOR="http://localhost:$SOUND_SUPERVISOR_PORT"
 while ! curl --silent --output /dev/null "$SOUND_SUPERVISOR/ping"; do sleep 5; echo "Waiting for audioblock to start..."; done
 
 # Start AirPlay
