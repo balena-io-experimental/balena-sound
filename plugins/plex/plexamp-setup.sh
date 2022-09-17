fe4c788c7ceeab40269ade86e03ea8aeb6938528
lewis <lewis@sniff122.tech>
2022-09-17 17:00:14 +0100

    Implement support for the headless PlexAmp client
    Added a container build for the headless PlexAmp client, this will allow
    a user to use the plex app on a mobile device to cast music from their
    media server to their Balena Sound device.
    
    Change-type: minor
    Signed-off-by: Lewis L. Foster <lewis@sniff122.tech>

---
+++ plugins/plex/plexamp-setup.sh
@@ -0,0 +1,5 @@
+wget "https://plexamp.plex.tv/headless/Plexamp-Linux-arm64-v4.2.2-beta.7.tar.bz2" -O "plexamp.tar.bz2"
+
+tar -xf plexamp.tar.bz2 
+
+
---
+++ plugins/plex/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -n "$SOUND_DISABLE_PLEX" ]]; then
+  echo "Plex is disabled, exiting..."
+  exit 0
+fi
+
+if [[ ! -d "/root/.local/share/Plexamp/Settings" ]]; then
+    echo "__________________________________________WARNING__________________________________________________"
+    echo "|                                  Plex is not configured!                                        |"
+    echo "| Please enter a shell into the Plex container and run `bash login.sh` to setup your credentials! |"
+    echo "|_________________________________________________________________________________________________|"
+    echo ""
+    echo "Note: This is likely to change as headless Plexamp versions are released with new features."
+    echo "  Passing a claim token via a command line argument has been suggested, we will have to see."
+    exit 1
+fi
+
+echo "Starting Plex plugin..."
+
+cd /opt/plexamp/plexamp/
+
+node js/index.js
