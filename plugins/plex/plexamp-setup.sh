031043c603744f44835da15ccee3ec93fdfee641
lewis <lewis@sniff122.tech>
2022-12-03 15:07:34 +0000

    bump plexamp headless version

--- plugins/plex/plexamp-setup.sh
+++ plugins/plex/plexamp-setup.sh
@@ -1,4 +1,4 @@
-wget "https://plexamp.plex.tv/headless/Plexamp-Linux-headless-v4.5.0.tar.bz2" -O "plexamp.tar.bz2"
+wget "https://plexamp.plex.tv/headless/Plexamp-Linux-headless-v4.6.0.tar.bz2" -O "plexamp.tar.bz2"
 
 tar -xf plexamp.tar.bz2 
 
