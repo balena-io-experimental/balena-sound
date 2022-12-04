a9c8fdb7052c98c35f3f968ed039a0a758ab9dbc
lewis <lewis@sniff122.tech>
2022-12-04 19:23:50 +0000

    automatically fetch the latest version of plexamp during build

--- plugins/plex/plexamp-setup.sh
+++ plugins/plex/plexamp-setup.sh
@@ -1,4 +1,11 @@
-wget "https://plexamp.plex.tv/headless/Plexamp-Linux-headless-v4.6.0.tar.bz2" -O "plexamp.tar.bz2"
+version_info=$(curl https://plexamp.plex.tv/headless/version.json)
+
+latest_version_number=$(echo $version_info | jq --raw-output '.latestVersion')
+latest_version_url=$(echo $version_info | jq --raw-output '.updateUrl')
+
+echo "Downloading PlexAmp version '$latest_version_number' from '$latest_version_url'"
+
+wget "$latest_version_url" -O "plexamp.tar.bz2"
 
 tar -xf plexamp.tar.bz2 
 
