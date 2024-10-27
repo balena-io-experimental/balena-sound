a01a62e7b5c625c2b37f24cb4f1809831e0ccc1d
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-27 20:23:10 +0100

    Add runtime configuration environment support for shairport

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -5,12 +5,25 @@ if [[ -n "$SOUND_DISABLE_AIRPLAY" ]]; then
   exit 0
 fi
 
+
 # --- ENV VARS ---
 # SOUND_DEVICE_NAME: Set the device broadcast name for AirPlay
 SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound AirPlay $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
 
 echo "Starting AirPlay plugin..."
 echo "Device name: $SOUND_DEVICE_NAME"
+
+# Check if LATENCY_OFFSET is set
+if [[ -n "$SOUND_AIRPLAY_LATENCY_OFFSET" ]]; then
+    # File to modify
+    file="/etc/shairport-sync.conf"
+
+    # Use sed to replace the value in the specified line
+    sed -i "s|//[[:space:]]*audio_backend_latency_offset_in_seconds[[:space:]]*=.*;|	audio_backend_latency_offset_in_seconds = $SOUND_AIRPLAY_LATENCY_OFFSET;|" "$file"
+
+    echo "Updated audio_backend_latency_offset_in_seconds to $LATENCY_OFFSET in $file"
+fi
+
 
 # Start AirPlay
 echo "Starting Shairport Sync"
